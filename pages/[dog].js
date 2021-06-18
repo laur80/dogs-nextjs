01fa333f2c9e77dac13dc92f05ee59ae9e75fe19
laur80 <laur80raul@gmail.com>
2021-06-18 11:05:47 +0300

    add loader

--- pages/[dog].js
+++ pages/[dog].js
@@ -7,26 +7,32 @@ function DogDetails() {
 	const id = router.query.dog;
 
 	function getDog(id) {
-		return dogs.find((dog) => dog.name === id);
+		let d = dogs.find((dog) => dog.name === id);
+		// console.log('dog', d);
+		return d;
 	}
 
 	let dog = getDog(id);
+	console.log(dog);
+	if (!dog) {
+		return 'tew';
+	}
 
 	return (
 		<div className='DogDetails row justify-content-center mt-5'>
 			<div className='col-11 col-lg-5'>
 				<div className='DogDetails-card card'>
-					<img className='card-img-top' src={dog.src} alt={dog.name} />
+					{/* <img className='card-img-top' src={dog.src} alt={dog.name} /> */}
 					<div className='card-body'>
 						<h2 className='card-title'>{dog.name}</h2>
 						<h4 className='card-subtitle text-muted'>{dog.age} years old</h4>
 					</div>
 					<ul className='list-group list-group-flush'>
-						{dog.facts.map((fact, i) => (
+						{dog.facts.map((fact, i) => {
 							<li className='list-group-item' key={i}>
 								{fact}
-							</li>
-						))}
+							</li>;
+						})}
 					</ul>
 					<div className='card-body'>
 						<Link href='/'>
